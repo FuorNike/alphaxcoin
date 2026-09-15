3304efb9a6dd349d238d3854e80cb03a9ebb8128
FuorNike <fuornike@gmail.com>
2026-09-15 20:00:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 BOT_TOKEN = "8619618419:AAFfKjA5UWOAxOekoflSmW2hGg05N5y3U-w"
 
 # Твоя готовая рабочая ссылка на тапалку:
-WEBAPP_URL = "https://github.io"?v=2
+WEBAPP_URL = "https://fuornike.github.io/alphaxcoin/"
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
